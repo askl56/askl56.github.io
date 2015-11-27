d9aea5f97f8ea306c1999ec491fa274ee20f322c
askl56 <amscott@openmailbox.org>
2015-11-27 17:16:49 +0000

    rubocop offenses fixed

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 # A sample Gemfile
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "jekyll", "~>2.5"
-gem "jekyll-archives", "~>2.0"
-gem "jekyll-sitemap", "~>0.8"
+gem 'jekyll', '~>2.5'
+gem 'jekyll-archives', '~>2.0'
+gem 'jekyll-sitemap', '~>0.8'
--- _plugins/rssgenerator.rb
+++ _plugins/rssgenerator.rb
@@ -38,15 +38,19 @@ module Jekyll
       parser = get_markdown_parser(site.config)
 
       # Create the rss with the help of the RSS module
-      rss = RSS::Maker.make("2.0") do |maker|
+      rss = RSS::Maker.make('2.0') do |maker|
         maker.channel.title = site.config['name']
         maker.channel.link = site.config['url']
         maker.channel.description = site.config['description'] || "RSS feed for #{site.config['name']}"
-        maker.channel.author = site.config["author"]
-        maker.channel.updated = site.posts.map { |p| p.date  }.max
+        maker.channel.author = site.config['author']
+        maker.channel.updated = site.posts.map(&:date).max
         maker.channel.copyright = site.config['copyright']
 
-        post_limit = (site.config['rss_post_limit'] - 1 rescue site.posts.count)
+        post_limit = (begin
+                        site.config['rss_post_limit'] - 1
+                      rescue
+                        site.posts.count
+                      end)
 
         site.posts.reverse[0..post_limit].each do |post|
           maker.items.new_item do |item|
@@ -59,11 +63,11 @@ module Jekyll
       end
 
       # File creation and writing
-      rss_path = ensure_slashes(site.config['rss_path'] || "/")
-      rss_name = site.config['rss_name'] || "rss.xml"
+      rss_path = ensure_slashes(site.config['rss_path'] || '/')
+      rss_name = site.config['rss_name'] || 'rss.xml'
       full_path = File.join(site.dest, rss_path)
       ensure_dir(full_path)
-      File.open("#{full_path}#{rss_name}", "w") { |f| f.write(rss) }
+      File.open("#{full_path}#{rss_name}", 'w') { |f| f.write(rss) }
 
       # Add the feed page to the site pages
       site.pages << Jekyll::RssFeed.new(site, site.dest, rss_path, rss_name)
@@ -86,7 +90,7 @@ module Jekyll
     #
     # Returns the path with a leading slash
     def ensure_leading_slash(path)
-      path[0] == "/" ? path : "/#{path}"
+      path[0] == '/' ? path : "/#{path}"
     end
 
     # Ensures the given path has a trailing slash
@@ -95,7 +99,7 @@ module Jekyll
     #
     # Returns the path with a trailing slash
     def ensure_trailing_slash(path)
-      path[-1] == "/" ? path : "#{path}/"
+      path[-1] == '/' ? path : "#{path}/"
     end
 
     # Ensures the given directory exists
@@ -113,21 +117,20 @@ module Jekyll
     #
     # Returns a parser or raises exception if one isn't found
     def get_markdown_parser(config)
-      return case config['markdown']
-        when 'redcarpet'
-          Jekyll::Converters::Markdown::RedcarpetParser.new config
-        when 'kramdown'
-          Jekyll::Converters::Markdown::KramdownParser.new config
-        when 'rdiscount'
-          Jekyll::Converters::Markdown::RDiscountParser.new config
-        when 'maruku'
-          Jekyll::Converters::Markdown::MarukuParser.new config
-        else
-          STDERR.puts "Invalid Markdown processor: #{config['markdown']}"
-          STDERR.puts "  Valid options are [ maruku | rdiscount | kramdown | redcarpet ]"
-          raise FatalException.new("Invalid Markdown process: #{config['markdown']}")
+      case config['markdown']
+      when 'redcarpet'
+        Jekyll::Converters::Markdown::RedcarpetParser.new config
+      when 'kramdown'
+        Jekyll::Converters::Markdown::KramdownParser.new config
+      when 'rdiscount'
+        Jekyll::Converters::Markdown::RDiscountParser.new config
+      when 'maruku'
+        Jekyll::Converters::Markdown::MarukuParser.new config
+      else
+        STDERR.puts "Invalid Markdown processor: #{config['markdown']}"
+        STDERR.puts '  Valid options are [ maruku | rdiscount | kramdown | redcarpet ]'
+        fail FatalException.new("Invalid Markdown process: #{config['markdown']}")
       end
     end
-
   end
 end
